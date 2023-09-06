f5eafdb837e90d02a48ff6bfe7c6d9e1682ed25c
David <129532735+djtbcode@users.noreply.github.com>
2023-09-06 19:40:49 +0100

    Update 1.1

--- pdfconverter.c
+++ pdfconverter.c
@@ -9,4 +9,18 @@ int main(int argc, char *argv[])
         printf("Usage: pdf2doc input.pdf output.doc\n");
         return 1;
    }
+   GError *error = NULL;
+   PopplerDocument *doc = poppler_document_new_from_file(argv[1], NULL, &error);
+   if (error != NULL) 
+   {
+      printf("Error opening PDF: %s\n", error->message);
+      g_error_free(error);
+      return 1;
+   }
+
+    // Extract text and formatting from the PDF here
+    // Create a DOC file and write the extracted content
+
+    poppler_document_free(doc);
+    return 0;
 }
