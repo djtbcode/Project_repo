a3dddc3fce2638d4e39dc6f954f4fd39ebe10607
David <129532735+djtbcode@users.noreply.github.com>
2023-09-08 12:36:29 +0100

    Update pdfconverter.c

--- pdfconverter.c
+++ pdfconverter.c
@@ -1,4 +1,5 @@
-#include <std.io>
+#include <stdio.h>
+#include <stdint.h> 
 #include <poppler/glib/poppler.h>
 
 
@@ -17,6 +18,33 @@ int main(int argc, char *argv[])
       g_error_free(error);
       return 1;
    }
+   // OPEN FILE
+    string filename = argv[1];
+    FILE *file = fopen(filename, "r");
+
+    // Check if file exists
+
+    uint8_t buffer[4];
+    unint8_t signature[] = {37, 80, 68, 70}
+    fread(buffer, 1, 4, file);
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%i", buffer[i]);
+    }
+    printf("\n");
+
+    for (int x = 0; x < 4; x++)
+    {
+        if (buffer[x] != signature[x])
+        {
+            printf("likely not PDF");
+            fclose(file);
+            return 0;
+        }
+    }
+    printf("likely a PDF! \n")
+    fclose(file);
+    return 0;
 
     // Extract text and formatting from the PDF here
     // Create a DOC file and write the extracted content
